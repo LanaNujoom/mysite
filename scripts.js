dadbd0aeae4e484a96d5972b23060ebf54093797
LanaNujoom <lanaalinujoom3@gmail.com>
2020-12-20 02:28:31 +0200

    Fix Undefined Error

--- scripts.js
+++ scripts.js
@@ -1,107 +1,102 @@
 alert("WELCOME");
 
-        var name = prompt("please enter your name");
-        alert("Hello " + name);
+var name = prompt("please enter your name");
+alert("Hello " + name);
 
 
 
-        var type = prompt("What Types of Birds are You Looking for?");
-        alert(type);
+var type = prompt("What Types of Birds are You Looking for?");
+alert(type);
 
 
 
-        var age = prompt("How Old Are You?");
+var age = prompt("How Old Are You?");
 
 
 
-        if (age > 15
-        ) {
-          alert("Welcome , you can enter website because you are older than 15");
-        } else {
-          alert("Sorry, Your age is less than 15");
+if (age > 15
+) {
+  alert("Welcome , you can enter website because you are older than 15");
+} else {
+  alert("Sorry, Your age is less than 15");
 
-        }
-
-
-
-        var languages = prompt("Please Enter Language");
-        var English = "English is Activated";
-        var Arabic = " Arabic is Activated";
-        var booleanVariableOfTypeTrue = true;
-
-        function language(MyLanguage) {
-          if (languages == "English") {
-
-            alert(English);
-
-          } else if (languages == "Arabic") {
-            alert(Arabic);
-          }
-        }
-        language(languages);
+}
 
 
 
-        if (true) {
-          alert("Welcome");
+var languages = prompt("Please Enter Language - Arabic or English");
+var English = "English is Activated";
+var Arabic = " Arabic is Activated";
+var booleanVariableOfTypeTrue = true;
 
-        }
+function language(MyLanguage) {
+  if (MyLanguage == "English") {
 
+    alert(English);
 
-        for (var i = 0; i < 10; i++) {
-
-          console.log(i);
-
-
-
-        }
-
-
-        console.log("Hello !");
-
-        let userinput = prompt('Do You love birds or cats?')
-
-        while (userinput !== 'birds') {
-
-          userinput = prompt(' Sorry This Site is for Birds Lover');
-        }
-
-        alert (' You are in the right place ');
+  } else if (MyLanguage == "Arabic") {
+    alert(Arabic);
+  }
+}
+language(languages);
 
 
 
-        alert(answer);
-
-        let howmany = function () {
 
 
-          let count = prompt(' How Many Birds You Want ?');
+for (var i = 0; i < 10; i++) {
 
-          while (count === '')
-
-            count = prompt(' Please Enter a Number');
-
-          return count;
-        }
-
-        let showorder = function () {
-
-          let item = '<img src= https://th.bing.com/th/id/OIP.Gr-4WOJw1qUFigeeCYTyrAHaH1?pid=Api&rs=1/>';
-          let total = howmany();
-          let result = '';
-
-          for (let i = 0; i < total; i++) {
-
-            result = result + i + item
-
-          }
-
-         
-          document.write(result);
+  console.log(i);
 
 
-        }
 
-        showorder () ;
+}
 
-        console.log
+
+console.log("Hello !");
+
+let userinput = prompt('Do You love birds or cats?')
+
+while (userinput !== 'birds') {
+
+  userinput = prompt(' Sorry This Site is for Birds Lover');
+}
+
+alert(' You are in the right place ');
+
+
+
+
+
+let howmany = function () {
+
+
+  let count = prompt(' How Many Birds You Want ?');
+
+  while (count === '')
+
+    count = prompt(' Please Enter a Number');
+
+  return count;
+}
+
+let showorder = function () {
+
+  let item = '<img src= https://th.bing.com/th/id/OIP.Gr-4WOJw1qUFigeeCYTyrAHaH1?pid=Api&rs=1/>';
+  let total = howmany();
+  let result = '';
+
+  for (let i = 0; i < total; i++) {
+
+    result = result + i + item
+
+  }
+
+ 
+  document.write(result);
+
+
+}
+
+showorder() ;
+
